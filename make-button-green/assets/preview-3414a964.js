ec12a0a81340135bc33534d3489bc160aa3f8402
juniorboos <juniorboos@users.noreply.github.com>
2023-05-08 16:02:14 +0000

    Deploying to gh-pages from  @ 440691fffc9a96611eb55b6782bd5263f7f35b97 🚀

---
+++ make-button-green/assets/preview-3414a964.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-22a189de.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-243cd309.js"),["./DocsRenderer-EYKKDMVH-243cd309.js","./iframe-22a189de.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./react-18-5792738b.js","./index-9900c05c.js","./index-a30bff61.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-3414a964.js.map
